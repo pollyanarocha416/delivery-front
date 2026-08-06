74e070726df0c4dc8dff9c6778eb642207a1e227
pollyanarocha416 <pollyanarocha416@gmail.com>
2026-08-06 18:43:21 -0400

    FIX: Adicionando setup para as rotas

--- front-delivery/src/App.vue
+++ front-delivery/src/App.vue
@@ -1,32 +1,15 @@
-<!-- <script setup>
-import Test from './components/Test.vue';
-import TheWelcome from './components/TheWelcome.vue'
+<script setup>
+import { useRoute } from 'vue-router';
+const route = useRoute()
 </script>
 
-<template>
-  <header>
-    
-      <Test msg="Bem vindo a pagina inicial de login e cadastro" />
-      <template>
-        <nav>
-          <router-link to="/segunda">Segunda</router-link>
-        </nav>
 
-        <router-view />
-      </template>
-  </header>
-
-  <router-view />
-
-  <main>
-    <TheWelcome />
-  </main>
-
-</template> -->
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/segunda">Segunda</router-link>
+    
+    <router-link v-if="route.path !== '/'" to="/">
+      voltar
+    </router-link>
   </nav>
 
   <router-view />
