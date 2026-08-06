4c1b668f39bc008149d3c13f937de18cde18cbe5
pollyanarocha416 <pollyanarocha416@gmail.com>
2026-08-06 18:42:22 -0400

    FEAT: Alterando nome da pagina

---
+++ front-delivery/src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>Login</h1>
+    <form>
+      <label for="username">Username:</label><br>
+      <input type="text" id="username" name="username"><br>
+      <label for="pwd">Password:</label><br>
+      <input type="password" id="pwd" name="pwd">
+    </form>
+  
+</template>
+
+<script setup>
+// código opcional
+</script>
+
+<style scoped>
+h1 {
+  color: #11e887;
+}
+label {
+  color: white;
+}
+input {
+  width: 300px;
+  height: 35px;
+  margin-bottom: 10px;
+}
+</style>
